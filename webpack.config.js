4d8bcdb4d69a56fb3ecbfec16de3b37e14e2971c
nofake1331 <nofake1331@gmail.com>
2020-11-10 21:15:47 +0200

    spa

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,25 @@
 var path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 module.exports = {
   //...
+  context: path.resolve(__dirname, 'src'),
+  entry: './main.js', 
   resolve: {
     alias: {
       vue: 'vue/dist/vue.js'
     }
   },
+  module:{
+    rules:[
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      }
+    ]
+  },
+  plugins:[
+    new VueLoaderPlugin()
+  ],
   devServer: {
     contentBase: path.join(__dirname, 'src'),
     compress: true,
